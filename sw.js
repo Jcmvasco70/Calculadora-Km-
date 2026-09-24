1918a1e7e7577987022125fad37b11c86dc84778
Jcmvasco70 <climacom360@gmail.com>
2026-09-24 12:54:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='calc-km-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='calc-km-v2';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icone-192.png','./icone-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
